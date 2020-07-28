a827ff3712ef713d42fa330a483e43d7e78281eb
Sofia Carballo <sofia.carballo@live.com>
2020-07-28 14:12:10 +0200

    Add relation between Permission & Role

--- app/Permission.php
+++ app/Permission.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Role;
 
 class Permission extends Model
 {
@@ -10,6 +11,6 @@ class Permission extends Model
     
     public function roles() {
 
-        return $this->belongsToMany(Roles::class);
+        return $this->belongsToMany(Role::class);
     }
 }
