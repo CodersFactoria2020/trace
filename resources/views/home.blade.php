7c2c454ea58ef08ed010452e45a2fc483753ee42
evalbors <evalbors@gmail.com>
2020-07-29 15:18:26 +0200

    applying style structuring by parts. Isues with icon in footer and hero slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,13 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="modul-intro-bg">
-    <div class="text-white">
-        <h1>Associació Catalana de Traumàtics Cranioencefàlics i Dany Cerebral</h1>
-        <p>Des de 1988, treballem per a millorar les seqüeles de les persones afectades 
-        de dany cerebral sobrevingut i la qualitat de vida tant d’elles 
-        com de les seves famílies.</p>
-    </div>
-</div>
-@endsection
+@extends('parts.footer')
+    @extends('parts.resources-section')    
+    @extends('parts.collaborators-section')
+    @extends('parts.team-section')
+    @extends('parts.services-section')
+    @extends('parts.description-section')
+    @extends('parts.hero')
+ @extends('layouts.app')
+
+
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,19 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="../resources/img/favicon.png" rel="icon">
-    <link href="resources/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="img/favicon.png" rel="icon">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./vendor/icofont/icofont.min.css" rel="stylesheet">
-    <link href="./vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="./vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="./vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="./vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="vendor/venobox/venobox.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
     <link href="css/app.css" rel="stylesheet">
@@ -36,18 +36,23 @@
 <body>
 
     <section id="topbar" class="d-none d-lg-block">
-        <div class="container d-flex align-items-center">
+        <div class="container">
+          <div class="d-flex align-items-center">
             <div class="contact-info mr-auto">
-              <i class="icofont-phone"></i> <a href="tel:+349332503636">933 250 3636</a>
-              <i class="icofont-envelope"></i><a href="mailto:info@tracecatalunya.org">info@tracecatalunya.org</a>
+                <a href="tel:+349332503636"><i class="icofont-phone"></i>933 250 3636</a>
+                <a href="mailto:info@tracecatalunya.org"><i class="icofont-envelope"></i>info@tracecatalunya.org</a>
             </div>
-            <div class="d-flex align-items-center justify-content-end social-links">
-              <p>Segueix-nos:</p>
-              <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-              <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-              <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-              <a href="#" class=""><i class="icofont-youtube"></i></a>
+            <div class="social-links">
+                <p>Segueix-nos:</p>
+                <a href="https://www.facebook.com/danycerebraltrace" target="_blank" class=""><i class="icofont-facebook"></i></a>
+                <a href="https://twitter.com/tracecatalunya?lang=es" target="_blank" class=""><i class="icofont-twitter"></i></a>
+                <a href="https://www.instagram.com/associaciotrace/" target="_blank" class=""><i class="icofont-instagram"></i></a>
+                <a href="https://www.youtube.com/channel/UCEXJ-1eKKkl8gqsqOBxy5dg" target="_blank"class=""><i class="icofont-youtube"></i></a>
             </div>
+          </div> 
+          <div class="user">
+            <a href="" target="_blank" class="cta-line">Àrea usuari</a>
+          </div>
         </div>
     </section>
 
@@ -90,40 +95,7 @@
    
 </body>
 
-<footer id="footer">
 
-<div class="footer-top">
-  <div class="container">
-    <div class="row">
-
-      <div class="col-lg-6 col-md-12 footer-links">
-        <div class="logo mr-auto">
-          <h1 class="text-light"><a href=""><span>traCE</span></a></h1>
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-        </div>
-      </div>
-
-      <div class="col-lg-6 col-md-12 d-flex align-items-center justify-content-end footer-info">
-        <div class="social-links">
-          <p>Segueix-nos:</p>
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class=""><i class="bx bxl-youtube"></i></a>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</div>
-
-<div class="container">
-  <div class="copyright">
-    &copy;2020 Associació Catalana de Traumàtics Cranioencefàlics i Dany Cerebral. All Rights Reserved.
-  </div>
-  
-</div>
-</footer><!-- End Footer -->
 
 <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
