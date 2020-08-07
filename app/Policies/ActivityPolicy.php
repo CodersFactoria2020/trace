2e4458f40f02ac78d3d7077aa6218cb31e99b2be
Elena <cabjelen@yahoo.es>
2020-08-07 11:36:49 +0200

    refactor activities permissions into ActivityPolicy | Elena

--- app/Policies/ActivityPolicy.php
+++ app/Policies/ActivityPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Activity;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
 
 class ActivityPolicy
 {
@@ -12,36 +13,54 @@ class ActivityPolicy
 
     public function viewAny(User $user)
     {
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
         return true;
     }
 
     public function view(User $user, Activity $activity)
     {
-        //
+        return true;
     }
 
     public function create(User $user)
     {
-        //
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
     }
 
     public function update(User $user, Activity $activity)
     {
-        //
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
     }
 
     public function delete(User $user, Activity $activity)
     {
-        //
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
     }
 
     public function restore(User $user, Activity $activity)
     {
-        //
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
     }
 
     public function forceDelete(User $user, Activity $activity)
     {
-        //
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
     }
 }
