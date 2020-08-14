1a14ddf30abd98e3d60f488920aecc3729f9c053
Elena <cabjelen@yahoo.es>
2020-08-14 12:35:49 +0200

    add category_color field to Category model | elena

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -7,11 +7,11 @@ class CategorySeeder extends Seeder
 {
     public function run()
     {
-        factory(Category::class)->create(['category_name'=>'Fisioteràpia neurològica', 'description'=>'Descripció de Fisioteràpia neurològica']);
-        factory(Category::class)->create(['category_name'=>'Neurologopèdia', 'description'=>'Descripció de Neurologopèdia']);
-        factory(Category::class)->create(['category_name'=>'Teràpia ocupacional', 'description'=>'Descripció de Teràpia ocupacional']);
-        factory(Category::class)->create(['category_name'=>'Neuropsicologia', 'description'=>'Descripció de Neuropsicologia']);
-        factory(Category::class)->create(['category_name'=>'GNPT i NEURONUP', 'description'=>'Descripció de GNPT i NEURONUP']);
-        factory(Category::class)->create(['category_name'=>'Grupals', 'description'=>'Descripció de Grupals']);
+        factory(Category::class)->create(['category_name'=>'Fisioteràpia neurològica', 'description'=>'Descripció de Fisioteràpia neurològica', 'category_color'=>'#84c9e6']);
+        factory(Category::class)->create(['category_name'=>'Neurologopèdia', 'description'=>'Descripció de Neurologopèdia', 'category_color'=>'#ebf556']);
+        factory(Category::class)->create(['category_name'=>'Teràpia ocupacional', 'description'=>'Descripció de Teràpia ocupacional', 'category_color'=>'#af8abc']);
+        factory(Category::class)->create(['category_name'=>'Neuropsicologia', 'description'=>'Descripció de Neuropsicologia', 'category_color'=>'#ff9a4d']);
+        factory(Category::class)->create(['category_name'=>'GNPT i NEURONUP', 'description'=>'Descripció de GNPT i NEURONUP', 'category_color'=>'#f9cdda']);
+        factory(Category::class)->create(['category_name'=>'Grupals', 'description'=>'Descripció de Grupals', 'category_color'=>'#35f631']);
     }
 }
