20b680ca0422534076f970a6501fa69b0e74969f
Elena <cabjelen@yahoo.es>
2020-08-14 17:50:05 +0200

    add background color to activity categories | elena

--- resources/views/activity/edit.blade.php
+++ resources/views/activity/edit.blade.php
@@ -41,7 +41,7 @@
                             <select name="category_id" class="form-control">
                                 <optgroup label="Selecciona una àrea">
                                 @foreach ($categories as $category)
-                                    <option value="{{ $category['id'] }}">{{ $category['category_name'] }}</option>
+                                    <option value="{{ $category['id'] }}" style="background-color:{{ $category['category_color'] }}">{{ $category['category_name'] }}</option>
                                 @endforeach
                             </select>
                         </div>
