941da0d78b63602c85e3c5f9b2901ca4d4c13f5d
Elena <cabjelen@yahoo.es>
2020-08-16 15:34:38 +0200

    removes middleware auth in HomeController | elena

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     public function index()
     {
