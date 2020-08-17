17e9863a484c71d4d970036f9d214fca499164fe
Kensyt <61829204+Kensyt@users.noreply.github.com>
2020-08-17 10:59:17 +0200

    edit show

--- resources/views/team/show.blade.php
+++ resources/views/team/show.blade.php
@@ -6,10 +6,15 @@
             <div class="card-header">
                 <h1> Membre de l'equip</h1><hr>
             </div>
-            <h2>ID:</h2><p>{{$team->id}}</p>
-            <h2>Name i Cognom:</h2><p>{{$team->first_name}} {{$team->last_name}}</p>
-            <h2>Description:</h2><p>{{$team->position}}</p>
-            <h2>Imatge:</h2><td> <img src="images/{{$team->photo}}" width="150" height="150"></td>
+            <div class="card-body">
+                <div class="flex-container">
+                    <div class="member">
+                        <img src="images/{{$team->photo}}" alt="" />
+                        <h4>{{$team->first_name}} {{$team->last_name}}</h4>
+                        <span>{{$team->position}}</span>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="card-footer">
             <a href="{{Route('team.index')}}" class="btn btn-secondary">Back</a>
