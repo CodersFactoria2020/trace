c258f1029f8580c13530c1f262f2742fdf50a23c
Kensyt <61829204+Kensyt@users.noreply.github.com>
2020-08-17 15:52:04 +0200

    Las modales ya estan configuradas le faltavan las politicas

--- app/Policies/TeamPolicy.php
+++ app/Policies/TeamPolicy.php
@@ -2,20 +2,74 @@
 
 namespace App\Policies;
 
+use App\Team;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
 
 class TeamPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function viewAny(User $user)
     {
-        //
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
+    }
+
+    public function view(User $user, Team $team)
+    {
+        return true;
+    }
+
+    public function edit(User $user, Team $team)
+    {
+        if (auth()->user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
+    }
+
+    public function create(User $user, Team $team)
+    {
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
+    }
+
+    public function update(User $user, Team $team)
+    {
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
+    }
+
+    public function destroy(User $user, Team $team)
+
+    {
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
+    }
+
+    public function restore(User $user, Team $team)
+    {
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
+    }
+
+    public function forceDelete(User $user, Team $team)
+    {
+        if (Auth::user()->role_id === "Soci") {
+            return false;
+        }
+        return true;
     }
 }
