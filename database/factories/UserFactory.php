299cb9e7a5dc9396188b6745f6b07da34290556d
Elena <cabjelen@yahoo.es>
2020-08-19 12:20:30 +0200

    add test states to UserFactory | elena

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Role;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -18,10 +19,17 @@ $factory->define(User::class, function (Faker $faker) {
     ];
 });
 
-// $factory->state(User::class, 'Admin', [
-//     'role_id' => 3,
-// ]);
+// TESTS - state - ADMIN Role 
+$factory->state(User::class, 'Admin', [
+    'role_id' => Role::$admin_id,
+]);
 
+// TESTS - state - PROFESSIONAL Role 
 $factory->state(User::class, 'Soci', [
-    'role_id' => 1,
+    'role_id' => Role::$professional_id,
+]);
+
+// TESTS - state - SOCI Role 
+$factory->state(User::class, 'Soci', [
+    'role_id' => Role::$soci_id,
 ]);
