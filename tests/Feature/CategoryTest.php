880efbaef9866f5e4563ffff8d49c83decdf1c8b
Sofia Carballo <sofia.carballo@live.com>
2020-08-20 17:39:16 +0200

    Add tests for Professional role permissions

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -39,7 +39,7 @@ class CategoryTest extends TestCase
         $response->assertRedirect('/areas');
     }
 
-    public function test_admin_can_access_one_category()
+    public function test_admin_can_view_one_category()
     {
         $role = factory(Role::class)->states('Admin')->create();
         $user = factory(User::class)->states('Admin')->create();
@@ -84,4 +84,70 @@ class CategoryTest extends TestCase
         $response->assertStatus(302);
         $response->assertRedirect('/areas');
     }
+
+    public function test_professional_can_view_all_categories()
+    {
+        $role = factory(Role::class)->states('Professional')->create();
+        $user = factory(User::class)->states('Professional')->create();
+        $response = $this->actingAs($user)->get('/areas');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_professional_cannot_create_category()
+    {
+        $role = factory(Role::class)->states('Professional')->create();
+        $user = factory(User::class)->states('Professional')->create();
+        $response = $this->actingAs($user)->post('/category', [
+            'id' => 1,
+            'name' => 'Neurology'
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function test_professional_can_view_one_category()
+    {
+        $role = factory(Role::class)->states('Professional')->create();
+        $user = factory(User::class)->states('Professional')->create();
+        $category = factory(Category::class)->create();
+        $response = $this->get('/category/'.$category->id);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_professional_cannot_update_category()
+    {
+        $role = factory(Role::class)->states('Professional')->create();
+        $user = factory(User::class)->states('Professional')->create();
+        $category = factory(Category::class)->create();
+
+        $response = $this->actingAs($user)->patch('/category/' . $category->id);
+
+        $this->assertDatabaseHas('categories', [
+            'id' => $category->id
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function test_professional_cannot_delete_category()
+    {
+        $admin_role = factory(Role::class)->states('Admin')->create();
+        $admin_user = factory(User::class)->states('Admin')->create();
+        $professional_role = factory(Role::class)->states('Professional')->create();
+        $professional_user = factory(User::class)->states('Professional')->create();
+        $category = factory(Category::class)->create();
+
+
+        $this->assertDatabaseHas('categories', [
+            'id' => $category->id
+        ]);
+
+        $response = $this->actingAs($professional_user)->delete('category/'. $category->id);
+
+        $response->assertStatus(403);
+    }
+
+
 }
