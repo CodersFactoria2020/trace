67d6c57137c8c9e44520b419771b2328a0703caa
Alejandro <alejandroacho@hotmail.com>
2020-08-20 23:22:22 +0200

    Contact | Contact form already works

--- public/vendor/php-email-form/php-email-form.php
+++ public/vendor/php-email-form/php-email-form.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * PHP Email Form
- * Version: 2.0
- * Website: https://bootstrapmade.com/php-email-form/
- * Copyright: BootstrapMade.com
- */
-
 if ( version_compare(PHP_VERSION, '5.5.0', '<') ) {
   die('PHP version 5.5.0 and up is required. Your PHP version is ' . PHP_VERSION);
 }
