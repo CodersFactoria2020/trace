996c0c85213b73ba68969ef2cbae5075455abc53
Sofia Carballo <sofia.carballo@live.com>
2020-08-20 18:09:58 +0200

    Refactor display empty list when there is any categories

--- resources/views/category/destroy.blade.php
+++ resources/views/category/destroy.blade.php
@@ -1,4 +1,4 @@
-<div  class="modal fadeIn" id="destroy-category{{$category->id ?? 'Default'}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div  class="modal fadeIn" id="destroy-category{{$category->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
 
@@ -10,16 +10,16 @@
             <div class="modal-body">
                 <p>Segur que desitja esborrar aquesta àrea?</p>
                 <div style="padding:5px;">
-                    <h5>Nom: {{$category->name ?? 'Default'}}</h5>
+                    <h5>Nom: {{$category->name}}</h5>
                 </div>
                 <div style="padding:5px;">
-                    <h5>descripció: {{$category->description ?? 'Default'}}</h5>
+                    <h5>descripció: {{$category->description}}</h5>
                 </div>
             </div>
 
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Cancel·lar</button>
-                <form action="{{Route('category.destroy', $category->id ?? 'Default')}}" method="POST">
+                <form action="{{Route('category.destroy', $category->id)}}" method="POST">
                     @csrf
                     @method('delete')
                     <input type="submit" class="btn btn-danger" value="Sí, esborrar!">
