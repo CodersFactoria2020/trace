6a0026345bd2b034d8fa71184f50064d9699a352
Sofia Carballo <sofia.carballo@live.com>
2020-08-20 16:02:26 +0200

    Refactor: rename tests, delete fields from array when creating,  use factory when creating instead of giving an array

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -12,7 +12,7 @@ class CategoryTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_admin_can_see_categories()
+    public function test_admin_can_view_all_categories()
     {
         $role = factory(Role::class)->states('Admin')->create();
         $user = factory(User::class)->states('Admin')->create();
@@ -27,16 +27,12 @@ class CategoryTest extends TestCase
         $user = factory(User::class)->states('Admin')->create();
         $response = $this->actingAs($user)->post('/category', [
             'id' => 1,
-            'name' => 'Neurology',
-            'description' => 'Is a branch of medicine dealing with disorders of the nervous system',
-            'color' => '#ff0000',
+            'name' => 'Neurology'
         ]);
 
         $this->assertDatabaseHas('categories', [
             'id' => 1,
-            'name' => 'Neurology',
-            'description' => 'Is a branch of medicine dealing with disorders of the nervous system',
-            'color' => '#ff0000',
+            'name' => 'Neurology'
         ]);
 
         $response->assertStatus(302);
@@ -47,12 +43,7 @@ class CategoryTest extends TestCase
     {
         $role = factory(Role::class)->states('Admin')->create();
         $user = factory(User::class)->states('Admin')->create();
-        $category = factory(Category::class)->create([
-            'id' => 1,
-            'name' => 'Neurology',
-            'description' => 'Is a branch of medicine dealing with disorders of the nervous system',
-            'color' => '#ff0000',
-        ]);
+        $category = factory(Category::class)->create();
 
         $response = $this->get('/category/'.$category->id);
 
@@ -65,18 +56,10 @@ class CategoryTest extends TestCase
         $user = factory(User::class)->states('Admin')->create();
         $category = factory(Category::class)->create();
 
-        $response = $this->actingAs($user)->patch('/category/' . $category->id, [
-            'id' => 1,
-            'name' => 'Psychology',
-            'description' => 'Is the science of mind and behavior',
-            'color' => '#ff1122',
-        ]);
+        $response = $this->actingAs($user)->patch('/category/' . $category->id);
 
         $this->assertDatabaseHas('categories', [
-            'id' => 1,
-            'name' => 'Psychology',
-            'description' => 'Is the science of mind and behavior',
-            'color' => '#ff1122',
+            'id' => $category->id
         ]);
 
         $response->assertStatus(302);
@@ -89,24 +72,16 @@ class CategoryTest extends TestCase
         $user = factory(User::class)->states('Admin')->create();
         $category = factory(Category::class)->create();
 
-        $this->assertDatabaseHas('categories', [
-            'id' => $category->id,
-            'name' => $category->name,
-            'description' => $category->description,
-            'color' => $category->color,
+        $this->assertDatabaseHas('categories', ['id' => $category->id
         ]);
 
         $response = $this->actingAs($user)->delete('category/'. $category->id);
 
         $this->assertDatabaseMissing('categories', [
             'id' => $category->id,
-            'name' => $category->name,
-            'description' => $category->description,
-            'color' => $category->color,
         ]);
 
         $response->assertStatus(302);
         $response->assertRedirect('/areas');
     }
-
 }
