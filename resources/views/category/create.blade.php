78d08ee2ee48d9002cea20fbee0d5a15a22b1e1a
evalbors <evalbors@gmail.com>
2020-08-24 19:28:30 +0200

    Afegir in cta of Categories

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="text-right">
-                            <input type="submit" value="Crear" class="cta">
+                            <input type="submit" value="Afegir" class="cta">
                         </div>
                         </div>
                     </div>
