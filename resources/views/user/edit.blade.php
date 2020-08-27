61c2312ee9c4105a40d45e97e7a30f91a756ca11
Sofia Carballo <sofia.carballo@live.com>
2020-08-27 16:47:09 +0200

    Fix validation

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -9,22 +9,22 @@
             </div>
             <div class="card-body">
                 <div class="modal-body">
-                    <form class="" action="{{Route('user.update', $user->id)}}" method="POST" class="needs-validation" novalidate>
+                    <form action="{{Route('user.update', $user->id)}}" method="POST" class="needs-validation" novalidate>
                         @csrf
                         @method('PUT')
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label class='label-small'>Nom</label>
-                                <input type="text" name="first_name" class="form-control" value="{{$user->first_name}}" required/>
+                                <input type="text" name="first_name" class="form-control" value="{{$user->first_name}}" pattern="[A-Za-z].{1,}" required/>
                                 <div class="invalid-feedback">
-                                    El nom ha de tenir almenys 3 caràcters
+                                    El nom ha de tenir almenys 1 lletra
                                 </div>
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Cognom</label>
-                                <input type="text" name="last_name" class="form-control" value="{{$user->last_name}}" required/>
+                                <input type="text" name="last_name" class="form-control" value="{{$user->last_name}}" pattern="[A-Za-z].{1,}" required/>
                                 <div class="invalid-feedback">
-                                    El cognom ha de tenir almenys 3 caràcters
+                                    El cognom ha de tenir almenys 1 lletra
                                 </div>
                             </div>
                         </div>
@@ -32,17 +32,17 @@
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label>Email</label>
-                                <input type="text" name="email" class="form-control" value="{{$user->email}}" required/>
+                                <input type="text" name="email" class="form-control" value="{{$user->email}}" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
                                 <div class="invalid-feedback">
-                                    El email no és vàlid
+                                    El email pot contenir lletres de llatí únic, números, '@' i '.'"
                                 </div>
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Contrasenya</label>
-                                <input type="text" name="dni" class="form-control" placeholder="Contrasenya" required/>
-                            </div>
-                            <div class="invalid-feedback">
-                                La contrasenya ha de tenir almenys 3 caràcters
+                                <input type="text" name="password" class="form-control" placeholder="Contrasenya" pattern=".{5,}" required/>
+                                <div class="invalid-feedback">
+                                    La contrasenya ha de tenir almenys 5 caràcters
+                                </div>
                             </div>
                         </div>
 
@@ -50,13 +50,16 @@
                             <div class="form-group col-md-6">
                                 <label>DNI</label>
                                 <input type="text" name="dni" class="form-control" value="{{$user->dni}}" required/>
-                                <div class="invalid-feedback">
-                                    El DNI ha de tenir almenys 9 caràcters
+                                <<div class="invalid-feedback">
+                                    El DNI ha de tenir almenys 5 caràcters alfanumèrics
                                 </div>
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Telèfon</label>
-                                <input type="text" name="phone" class="form-control" value="{{$user->phone}}" required/>
+                                <input type="text" name="phone" class="form-control" value="{{$user->phone}}" required pattern="[0-9].{6,}"/>
+                                <div class="invalid-feedback">
+                                    El telèfon ha de tenir almenys 6 números
+                                </div>
                             </div>
                         </div>
 
@@ -72,7 +75,10 @@
                             </div>
                             <div class="form-group col-md-6">
                                 <label>Tutor(a)</label>
-                                <input type="text" name="tutor" class="form-control" value="{{$user->tutor}}"/>
+                                <input type="text" name="tutor" class="form-control" value="{{$user->tutor}}" pattern="[A-Za-z]"/>
+                            </div>
+                            <div class="invalid-feedback">
+                                El tutor ha de tenir solament lletras
                             </div>
                         </div>
 
