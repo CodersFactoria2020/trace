5fec375adb2d10d274ef8c2c57b3c2db9a74857c
Sofia Carballo <sofia.carballo@live.com>
2020-08-29 19:01:55 +0200

    Add href to contact info, delete spaces

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\PHPprojects\trace\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\PHPprojects\trace\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\FactoriaF5\Projects\FactoriaF5\trace\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\FactoriaF5\Projects\FactoriaF5\trace\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
