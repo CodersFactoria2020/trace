33a5151fc9e844077c8382386c7f7e4e2abf9f70
Elena <cabjelen@yahoo.es>
2020-08-31 21:20:02 +0200

    add Catalan locale to datetimepicker in Activity

--- resources/views/activity/create.blade.php
+++ resources/views/activity/create.blade.php
@@ -3,7 +3,8 @@
 <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
+<script type="text/javascript" src="js/moment/ca.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
     
     <!-- Font Awesome CSS -->
@@ -141,8 +142,8 @@
 <!-- Datepicker Script -->
 <script type="text/javascript">
 $(function(){
- $('#datetimepicker1').datetimepicker();
- $('#datetimepicker2').datetimepicker(); 
+ $('#datetimepicker1').datetimepicker({locale:'ca'});
+ $('#datetimepicker2').datetimepicker({locale:'ca'}); 
 });
 </script>
 
