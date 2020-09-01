6de6268cfe62ce42c872e7d2b15d0eeefe6a99b2
Elena <cabjelen@yahoo.es>
2020-09-01 12:46:29 +0200

    add Soci filter to Activity create modal | Elena

--- resources/views/activity/create.blade.php
+++ resources/views/activity/create.blade.php
@@ -6,11 +6,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
 <script type="text/javascript" src="js/moment/ca.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-    
+
     <!-- Font Awesome CSS -->
     <link href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.css' rel='stylesheet'>
 
-    
+
 </head>
 <div class="modal fade" id="create-activity" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
@@ -103,26 +103,19 @@
                                     </div>
                                 </div>
                             </div>
-
                             <div class="form-row">
                                 <div class="col-md-6">
                                     <label>Socis:</label><br>
-                                    <div class="dropdown">
-                                        <button style="padding-left:0px" class="btn form-control" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-                                            <span class="glyphicon glyphicon-menu-down"></span>
-                                            Selecciona un soci
-                                            <i style="padding-left:125px" class="fas fa-angle-down"></i>
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <div>
+                                    <input class="form-control" id="myInput" type="text" placeholder="Buscar...">
+                                        <ul aria-labelledby="dropdownMenuButton"  id="socisList">
                                             @foreach ($socis as $soci)
-                                            <input class="mr-1 mb-3 ml-3" type="checkbox" value="{{ $soci['id'] }}" name="socis[]"> {{ $soci['first_name'] }} <br>
+                                            <li class="mr-1 mb-3 ml-3" style="list-style-type: none;display:none;"><input type="checkbox" value="{{ $soci['id'] }}" name="socis[]"> {{ $soci['first_name'] }}</li>
                                             @endforeach
-                                        </div>
+                                        </ul>
                                     </div>
                                 </div>
-                            </div>
-
-                            <div class="form-row pt-2">
+                            </div><div class="form-row pt-2">
                                 <div class="form-group col-md-6 pt-2">
                                     <label>Document adjunt:</label>
                                     <input type="file" name="file" id="fileToUpload"/>
@@ -143,7 +136,19 @@
 <script type="text/javascript">
 $(function(){
  $('#datetimepicker1').datetimepicker({locale:'ca'});
- $('#datetimepicker2').datetimepicker({locale:'ca'}); 
+ $('#datetimepicker2').datetimepicker({locale:'ca'});
+});
+</script>
+
+<!-- Socis List filter Script -->
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#socisList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 });
 </script>
 
